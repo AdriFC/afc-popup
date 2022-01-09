2267d73abf9e664df0e015e03b230ace7462e6b7
AdriFC <adri86vigo@gmail.com>
2022-01-09 13:23:03 +0100

    Terminado encolar estilos y scripts, error en alert.

---
+++ admin/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * Archivo js de la parte de la administración
+*/
+
+//Evitar conflitos del $ de jquery
+$ = jQuery.noConflict();
+
+//Zona jquery
+$(document).ready(function(){
+
+    alert('Ventana de prueba');
+
+});
+
+alert('testing!');
--- partials/afc-archivos.php
+++ partials/afc-archivos.php
@@ -3,92 +3,122 @@
  *  Aquí encolamos todos los archivos css y js
  */
 
- function enqueue_style($hook){
-    if($hook != 'toplevel_page_afc_popup'){
-        return;
+    function enqueue_style($hook){
+        if($hook != 'toplevel_page_afc_popup'){
+            return;
+        }
+
+        //Encolando css
+        wp_enqueue_style(
+            'admin-style',
+            plugin_dir_url(__DIR__) . 'admin/css/app.css',
+            [],
+            '1.0.0',
+            'all'
+        );
+
+        //Encolando bootstrap 5
+        wp_enqueue_style(
+            'bootstrap-css',
+            plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap.min.css',
+            [],
+            '5.1.0',
+            'all'
+        );
+
+        // wp_enqueue_style(
+        //     'bootstrap-rtl-css',
+        //     plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap.rtl.min.css',
+        //     [],
+        //     '5.1.0',
+        //     'all'
+        // );
+
+        // wp_enqueue_style(
+        //     'bootstrap-grid-css',
+        //     plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-grid.min.css',
+        //     [],
+        //     '5.1.0',
+        //     'all'
+        // );
+
+        // wp_enqueue_style(
+        //     'bootstrap-grid-rtl-css',
+        //     plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-grid.rtl.min.css',
+        //     [],
+        //     '5.1.0',
+        //     'all'
+        // );
+
+        // wp_enqueue_style(
+        //     'bootstrap-reboot-css',
+        //     plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-reboot.min.css',
+        //     [],
+        //     '5.1.0',
+        //     'all'
+        // );
+
+        // wp_enqueue_style(
+        //     'bootstrap-reboot-rtl-css',
+        //     plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-reboot.rtl.min.css',
+        //     [],
+        //     '5.1.0',
+        //     'all'
+        // );
+
+        // wp_enqueue_style(
+        //     'bootstrap-utilities-css',
+        //     plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-utilities.min.css',
+        //     [],
+        //     '5.1.0',
+        //     'all'
+        // );
+
+        // wp_enqueue_style(
+        //     'bootstrap-utilities-rtl-css',
+        //     plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-utilities.rtl.min.css',
+        //     [],
+        //     '5.1.0',
+        //     'all'
+        // );
     }
+    add_action('admin_enqueue_scripts', 'enqueue_style');
 
-    //Encolando css
-    wp_enqueue_style(
-        'admin-style',
-        plugin_dir_url(__DIR__) . 'admin/css/app.css',
-        [],
-        '1.0.0',
-        'all'
-    );
+    function enqueue_scripts($hook){
+        if($hook != 'toplevel_page_afc_popup'){
+            return;
+        }
 
-    //Encolando bootstrap 5
-    wp_enqueue_style(
-        'bootstrap-css',
-        plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap.min.css',
-        [],
-        '5.1.0',
-        'all'
-    );
+        //Encolando JS
+        wp_enqueue_script(
+            'admin-script',
+            plugin_dir_url(__DIR__) . 'admin/js/app.js',
+            ['jquery', 'bootstrap-css'],
+            '1.0.0',
+            true
+        );
 
-    wp_enqueue_style(
-        'bootstrap-rtl-css',
-        plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap.rtl.min.css',
-        [],
-        '5.1.0',
-        'all'
-    );
+        //Encolando JS bootstrap
+        wp_enqueue_script(
+            'bootstrap-js',
+            plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/js/bootstrap.min.js',
+            ['jquery'],
+            '5.1',
+            true
+        );
 
-    wp_enqueue_style(
-        'bootstrap-grid-css',
-        plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-grid.min.css',
-        [],
-        '5.1.0',
-        'all'
-    );
+        wp_enqueue_script(
+            'bootstrap-bundle',
+            plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/js/bootstrap.bundle.min.js',
+            ['jquery'],
+            '5.1',
+            true
+        );
 
-    wp_enqueue_style(
-        'bootstrap-grid-rtl-css',
-        plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-grid.rtl.min.css',
-        [],
-        '5.1.0',
-        'all'
-    );
-
-    wp_enqueue_style(
-        'bootstrap-reboot-css',
-        plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-reboot.min.css',
-        [],
-        '5.1.0',
-        'all'
-    );
-
-    wp_enqueue_style(
-        'bootstrap-reboot-rtl-css',
-        plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-reboot.rtl.min.css',
-        [],
-        '5.1.0',
-        'all'
-    );
-
-    wp_enqueue_style(
-        'bootstrap-utilities-css',
-        plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-utilities.min.css',
-        [],
-        '5.1.0',
-        'all'
-    );
-
-    wp_enqueue_style(
-        'bootstrap-utilities-rtl-css',
-        plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.1/css/bootstrap-utilities.rtl.min.css',
-        [],
-        '5.1.0',
-        'all'
-    );
- }
- add_action('admin_enqueue_scripts', 'enqueue_style');
-
- function enqueue_scripts($hook){
-    if($hook != 'toplevel_page_afc_popup'){
-        return;
     }
- }
+    add_action('admin_enqueue_scripts', 'enqueue_scripts')
+
+ 
 
 
 ?>
